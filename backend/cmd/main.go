b9eef0c877a6b5c199230006dd6d39610b036978
Eugene Yarshevich <yarshevich@gmail.com>
2022-06-13 17:57:31 +0300

    A very basic backend service that listens for incoming HTTP requests

---
+++ backend/cmd/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/mitchellh/mapstructure"
+	"github.com/spf13/viper"
+)
+
+// Configuration ...
+type configuration struct {
+	Host    string `mapstructure:"HOST"`
+	Port    int    `mapstructure:"PORT"`
+	Debug   bool   `mapstructure:"DEBUG"`
+	ConnStr string `mapstructure:"CONNECTION_STRING"`
+}
+
+var (
+	conf = &configuration{
+		Host: "",
+		Port: 8080,
+	}
+)
+
+func printConf(w http.ResponseWriter, req *http.Request) {
+	rawJson, err := json.Marshal(conf)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(rawJson)
+}
+
+func main() {
+	if err := loadConfig(conf); err != nil {
+		log.Fatalf(`Failed to load application configuration: %v`, err)
+	}
+
+	addr := fmt.Sprintf("%s:%d", conf.Host, conf.Port)
+	log.Printf("Starting server on: '%s'\n", addr)
+	http.HandleFunc("/", printConf)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func loadConfig(conf *configuration) error {
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	// Workaround for the viper to use environment variables without reading a config file
+	// Viper #761: Unmarshal non-bound environment variables
+	// https://github.com/spf13/viper/issues/761
+	envKeysMap := &map[string]interface{}{}
+	if err := mapstructure.Decode(conf, &envKeysMap); err != nil {
+		return err
+	}
+	for k := range *envKeysMap {
+		if err := viper.BindEnv(k); err != nil {
+			return err
+		}
+	}
+	// END (Workaround)
+
+	if err := viper.Unmarshal(conf); err != nil {
+		return err
+	}
+
+	validate := validator.New()
+	return validate.Struct(conf)
+}
